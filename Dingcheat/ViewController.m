4a45cfe4e3507f49214466e8d4b4abdfe8bdfb42
qifeibo <616532237@qq.com>
2019-04-29 13:32:21 +0800

    增加地理位置

--- Dingcheat/ViewController.m
+++ Dingcheat/ViewController.m
@@ -16,8 +16,25 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view.
+    //获取路径对象
+    NSArray *paths = NSSearchPathForDirectoriesInDomains(NSDocumentDirectory,NSUserDomainMask,YES);
+    //获取完整路径
+    NSString *documentsDirectory = [paths objectAtIndex:0];
+    
+    NSString *plistPath = [documentsDirectory stringByAppendingPathComponent:@"Location.gpx"];
+    
+    NSMutableDictionary *dictplist = [[NSMutableDictionary alloc] init];
+    
+    NSError *error;
+    NSString *textFieldContents=[NSString stringWithContentsOfFile:[[NSBundle mainBundle] pathForResource:@"Locationx" ofType:@"gpx"] encoding:NSUTF8StringEncoding error:&error];
+    NSLog(@"--textFieldContents---%@-----",textFieldContents);
+    if (textFieldContents==nil) {
+        NSLog(@"---error--%@",[error localizedDescription]);
+    }
+    NSArray *lines=[textFieldContents componentsSeparatedByString:@"\n"];
+    NSLog(@"number of lines in the file:%ld",[lines count]);
 }
+
 
 
 @end
